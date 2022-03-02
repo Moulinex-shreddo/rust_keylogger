68f3fcbbc40af0106f3010ba2e766d7bf2d729be
mremond <matthieu.remond@sfr.fr>
2022-03-02 17:58:01 +0100

    smol changes

--- src/main.rs
+++ src/main.rs
@@ -25,44 +25,37 @@ fn service_main(arguments: Vec<OsString>) {
 fn run_service(_arguments: Vec<OsString>) -> Result<(), windows_service::Error> {
     let event_handler = move |control_event| -> ServiceControlHandlerResult {
         match control_event {
-            ServiceControl::Stop => {
-                // Handle stop event and return control back to the system.
-                ServiceControlHandlerResult::NoError
-            }
-            // All services must accept Interrogate even if it's no-op.
-            ServiceControl::Interrogate => ServiceControlHandlerResult::NoError,
+            ServiceControl::Stop | ServiceControl::Interrogate => ServiceControlHandlerResult::NoError,
             _ => ServiceControlHandlerResult::NotImplemented,
         }
     };
 
     // Register system service event handler.
-    let status_handle = service_control_handler::register("totally spies", event_handler)?;
+    let status_handle = service_control_handler::register("totally_spies", event_handler)?;
 
     let next_status = ServiceStatus {
-        // Should match the one from system service registry.__rust_force_expr!
+        // Should match the one from system service registry
         service_type: ServiceType::OWN_PROCESS,
 
-        // The new state.
+        // The new state
         current_state: ServiceState::Running,
 
-        // Accept stop events when running.
+        // Accept stop events when running
         controls_accepted: ServiceControlAccept::STOP,
 
-        // Used to report an error when starting or stopping only, otherwise must be zero.
+        // Used to report an error when starting or stopping only, otherwise must be zero
         exit_code: ServiceExitCode::Win32(0),
 
         // Only used for pending states, otherwise must be zero
-        checkpoint: 0, 
+        checkpoint: 0,
         wait_hint: Duration::default(),
 
         // Process ID
         process_id: None,
     };
 
-    // Tell the system that the service is running now.
+    // Tell the system that the service is running now
     status_handle.set_service_status(next_status)?;
-
-    println!("Hello world!");
 
     Ok(())
 }
